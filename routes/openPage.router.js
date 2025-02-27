3539a6a75c20128511b2440ac07e0719a151459e
LedesmaFrancoAgustin <ledesmafranco50@gmail.com>
2025-02-26 22:47:56 -0300

    Agregado boton Comprar/puppeteerV.2

--- routes/openPage.router.js
+++ routes/openPage.router.js
@@ -5,49 +5,17 @@ import mongoose from "mongoose";
 import SessionCookie from "../models/sessionCookies.model.js"; // Modelo de MongoDB
 
 const router = express.Router();
-
-router.get('/open-socio', async (req, res) => {
-  try {
-    console.log("🔹 Ruta '/open-socio' llamada");
-
-    const email = "ledesma-agustin@hotmail.com";
-    if (!email) {
-      console.log("⚠️ Email no proporcionado");
-      return res.status(400).send("Email requerido");
-    }
-
-    console.log(`🔹 Buscando sesión para: ${email}`);
-    const sessionData = await SessionCookie.findOne({ email });
-
-    if (!sessionData) {
-      console.log("⚠️ No se encontraron datos de sesión en MongoDB");
-      return res.status(404).send("No se encontraron datos de sesión");
-    }
-
-    console.log("✅ Datos de sesión encontrados:", sessionData);
-
-    const browser = await puppeteer.launch({
-      args: chromium.args,
-      executablePath: await chromium.executablePath(),
-      headless: chromium.headless
-  });
+router.get("/redirect-boca", async (req, res) => {
+  // Buscar cookies de MongoDB
+  const session = await SessionCookie.findOne({ email: "ledesma-agustin@hotmail.com" });
   
-  
-
-    console.log("✅ Navegador iniciado");
-    const page = await browser.newPage();
-    await page.goto("https://bocasocios.bocajuniors.com.ar/auth/login", {
-      waitUntil: "domcontentloaded", // ⚡ Carga más rápido
-      timeout: 10000, // ⏳ Evita bloqueos largos
-      headless: false
-  });
-
-  
-
-  } catch (error) {
-    console.error("❌ Error al iniciar sesión:", error);
-    res.status(500).send("Error al abrir la página");
+  if (!session) {
+      return res.status(403).send("No hay sesión guardada.");
   }
+
+  res.setHeader("Set-Cookie", `authToken=${session.localStorage.authToken}; Path=/; HttpOnly`);
+  res.redirect("https://bocasocios.bocajuniors.com.ar/auth/login");
 });
+
 
 export default router;
