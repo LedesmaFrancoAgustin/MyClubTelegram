105506a459865509a675c05808aaaa6b6d10d0e7
LedesmaFrancoAgustin <ledesmafranco50@gmail.com>
2025-02-26 22:57:16 -0300

    Agregado boton Comprar/puppeteerV.2

--- routes/openPage.router.js
+++ routes/openPage.router.js
@@ -6,40 +6,21 @@ import SessionCookie from "../models/sessionCookies.model.js"; // Modelo de Mong
 
 const router = express.Router();
 router.get("/redirect-boca", async (req, res) => {
-  // Buscar cookies de MongoDB
   const session = await SessionCookie.findOne({ email: "ledesma-agustin@hotmail.com" });
 
   if (!session) {
       return res.status(403).send("No hay sesión guardada.");
   }
 
-  // Enviar un HTML con un script que restaura `localStorage` y `sessionStorage`
-  res.send(`
-      <html>
-      <head>
-          <title>Redireccionando...</title>
-          <script>
-              (function() {
-                  // Restaurar LocalStorage
-                  const localData = ${JSON.stringify(session.localStorage)};
-                  Object.keys(localData).forEach(key => localStorage.setItem(key, localData[key]));
+  // Inyectar el localStorage como un script en la respuesta
+  const script = `
+    <script>
+      localStorage.setItem('boca-secure-storage\\\\authStore', '${session.localStorage["boca-secure-storage\\authStore"]}');
+      window.location.href = "https://bocasocios.bocajuniors.com.ar/auth/login";
+    </script>
+  `;
 
-                  // Restaurar SessionStorage
-                  const sessionData = ${JSON.stringify(session.sessionStorage)};
-                  Object.keys(sessionData).forEach(key => sessionStorage.setItem(key, sessionData[key]));
-
-                  // Redirigir a Boca
-                  setTimeout(() => {
-                      window.location.href = "https://bocasocios.bocajuniors.com.ar/auth/login";
-                  }, 1000);
-              })();
-          </script>
-      </head>
-      <body>
-          <p>Redirigiendo a Boca Juniors...</p>
-      </body>
-      </html>
-  `);
+  res.send(script);
 });
 
 export default router;
