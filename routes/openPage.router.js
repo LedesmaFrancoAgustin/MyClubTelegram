880a80fb4f23266f32d45f0223d074d8db82ecd2
LedesmaFrancoAgustin <ledesmafranco50@gmail.com>
2025-02-26 21:43:48 -0300

    Agregado boton Comprar/puppeteerV.2

--- routes/openPage.router.js
+++ routes/openPage.router.js
@@ -1,27 +1,22 @@
 import express from "express";
-import puppeteer from "puppeteer-core";
-import chromium from "@sparticuz/chromium";
-
 
 const router = express.Router();
 
 router.get('/open-socio', async (req, res) => {
   try {
-      const browser = await puppeteer.launch({
-        args: chromium.args,
-        executablePath: await chromium.executablePath(),
-        headless: false,  // Ver la ejecución en una ventana
-    });
-  
-
-      const page = await browser.newPage();
-      await page.goto('https://bocasocios.bocajuniors.com.ar/auth/login');
-
-
-      res.send('Sesión iniciada en SoySocio');
+    res.send(`
+      <html>
+        <head>
+          <title>SoySocio</title>
+        </head>
+        <body>
+          <iframe src="https://bocasocios.bocajuniors.com.ar/auth/login" width="100%" height="800px" style="border:none;"></iframe>
+        </body>
+      </html>
+    `);
   } catch (error) {
-      console.error('Error al iniciar sesión:', error);
-      res.status(500).send('Error al abrir la página');
+    console.error('Error al cargar la página:', error);
+    res.status(500).send('Error al abrir la página');
   }
 });
 
